20f2d1860bae36eb01d65d6b221f8d5d0495f57d
Yuwei <49397791+YuWei-CH@users.noreply.github.com>
2023-04-28 18:07:35 -0400

    update

--- Project-code/backend-server1.py
+++ Project-code/backend-server1.py
@@ -1,6 +1,7 @@
 # Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
+from datetime import datetime
 
 # Initialize the app from Flask
 app = Flask(__name__)
@@ -10,7 +11,7 @@ conn = pymysql.connect(host='localhost',
                        port=8889,
                        user='root',
                        password='root',
-                       db='blog',
+                       db='Test-flight',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
@@ -23,16 +24,48 @@ def hello():
 # Define route for login
 
 
-@app.route('/login')
+@app.route('/User/user-login.html')
 def login():
-    return render_template('login.html')
+    return render_template('User/user-login.html')
 
 # Define route for register
 
 
-@app.route('/register')
+@app.route('/User/user-register.html')
 def register():
-    return render_template('register.html')
+    return render_template('User/user-register.html')
+
+# query future flight
+
+
+@app.route('/search_flights', methods=['POST'])
+def search_flights():
+    # get data from form
+    airline_name = request.form['airline_name']
+    flight_number = request.form['flight_number']
+    departure_date_and_time = request.form['departure_date_and_time']
+    arrival_date_and_time = request.form['arrival_date_and_time']
+    arrival_airport_code = request.form['arrival_airport_code']
+    departure_airport_code = request.form['departure_airport_code']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = "SELECT * FROM Flight WHERE airline_name=%s OR flight_number=%s OR departure_date_and_time=%s OR arrival_date_and_time=%s OR arrival_airport_code=%s OR departure_airport_code=%s"
+    cursor.execute(query, (airline_name, flight_number, departure_date_and_time,
+                   arrival_date_and_time, arrival_airport_code, departure_airport_code))
+    # stores the results in a variable
+    flights = cursor.fetchall()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    if len(flights) == 0:
+        # no flights
+        error = 'No flights found for the selected criteria.'
+        return render_template('index.html', error=error)
+
+    # Return result
+    return render_template('index.html', flights=flights)
+
 
 # Authenticates the login
 
@@ -40,53 +73,63 @@ def register():
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
     # grabs information from the forms
-    username = request.form['username']
+    email = request.form['email']
     password = request.form['password']
 
     # cursor used to send queries
     cursor = conn.cursor()
     # executes query
-    query = 'SELECT * FROM user WHERE username = %s and password = %s'
-    cursor.execute(query, (username, password))
+    query = 'SELECT * FROM Customer WHERE email_address = %s and c_password = %s'
+    cursor.execute(query, (email, password))
     # stores the results in a variable
     data = cursor.fetchone()
     # use fetchall() if you are expecting more than 1 data row
+    # search name
+    name_query = 'SELECT first_name FROM Customer WHERE email_address = %s and c_password = %s'
+    cursor.execute(name_query, (email, password))
+    # stores the results in a variable
+    name = cursor.fetchone()
     cursor.close()
     error = None
     if (data):
         # creates a session for the the user
         # session is a built in
-        session['username'] = username
-        return redirect(url_for('home'))
+        session['name'] = name
+        session['email'] = email
+        return redirect(url_for('user_main'))
     else:
         # returns an error message to the html page
-        error = 'Invalid login or username'
+        error = 'Invalid login or email'
         return render_template('login.html', error=error)
 
+
 # Authenticates the register
-
-
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-    # 获取 HTML 表单提交的数据
-    name = request.form['name']
-    email = request.form['email']
-    password = request.form['password']
-    address = request.form['address']
-    phone_number = request.form['phone_number']
-    passport_number = request.form['passport_number']
-    passport_expiration = request.form['passport_expiration']
-    passport_country = request.form['passport_country']
-    date_of_birth = request.form['date_of_birth']
-    # grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        # grabs information from the forms
+        email = request.form['email']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        building_number = request.form['building_number']
+        street_name = request.form['street_name']
+        apartment_number = request.form['apartment_number']
+        city = request.form['city']
+        state = request.form['state']
+        zip_code = request.form['zip_code']
+        phone_number1 = request.form['phone_number1']
+        phone_number2 = request.form['phone_number2']
+        passport_number = request.form['passport_number']
+        passport_expiration = request.form['passport_expiration']
+        passport_country = request.form['passport_country']
+        date_of_birth = request.form['date_of_birth']
 
     # cursor used to send queries
     cursor = conn.cursor()
     # executes query
-    query = 'SELECT * FROM user WHERE username = %s'
-    cursor.execute(query, (username))
+    query = 'SELECT * FROM Customer WHERE email_address = %s'
+    cursor.execute(query, (email))
     # stores the results in a variable
     data = cursor.fetchone()
     # use fetchall() if you are expecting more than 1 data row
@@ -96,8 +139,9 @@ def registerAuth():
         error = "This user already exists"
         return render_template('register.html', error=error)
     else:
-        ins = 'INSERT INTO user VALUES(%s, %s)'
-        cursor.execute(ins, (username, password))
+        ins = "INSERT INTO Customer (email_address,first_name, last_name, c_password, building_number, street_name, apartment_number, city, state, zip_code, phone_number1, phone_number2, passport_number, passport_expiration, passport_country, date_of_birth) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s)"
+        cursor.execute(ins, (email, first_name, last_name, password, building_number, street_name, apartment_number, city,
+                             state, zip_code, phone_number1, phone_number2, passport_number, passport_expiration, passport_country, date_of_birth))
         conn.commit()
         cursor.close()
         return render_template('index.html')
@@ -111,21 +155,21 @@ def logout():
 '''
 
 
-@app.route('/User/user-main.html', methods=['GET', 'POST'])
-def get_future_ticket():
-    # temp data
-    flights = [
-        {'flight_number': 'CA123', 'departure_date': '2023-05-01',
-         'arrival_date': '2023-05-01', 'departure_airport': 'PEK', 'arrival_airport': 'JFK'},
-        {'flight_number': 'MU456', 'departure_date': '2023-05-01',
-         'arrival_date': '2023-05-01', 'departure_airport': 'SHA', 'arrival_airport': 'LAX'},
-        {'flight_number': 'CZ789', 'departure_date': '2023-05-02',
-         'arrival_date': '2023-05-02', 'departure_airport': 'CAN', 'arrival_airport': 'SFO'}
-    ]
-    return render_template('User/user-main.html', flights=flights)
+@app.route('/user-main', methods=['GET', 'POST'])
+def user_main():
+    username = session['name']
+    email = session['email']
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM Ticket JOIN Flight WHERE email_address = %s AND Ticket.flight_number = Flight.flight_number'
+    cursor.execute(query, (email))
+    # stores the results in a variable
+    flights = cursor.fetchall()
+    return render_template('User/user-main.html', username=username, flights=flights)
 
 
-@app.route('/User/user-flight-booking.html', methods=['GET', 'POST'])
+@app.route('/user-flight-booking.html', methods=['GET', 'POST'])
 def get_user_ticket():
     # temp data
     flights = [
