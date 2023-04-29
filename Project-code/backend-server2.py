8edb89f59db9378c2a32649fcbdb0e2a97352486
Yirong Wang <yw5490@nyu.edu>
2023-04-29 02:44:37 -0400

    2023.4.29

--- Project-code/backend-server2.py
+++ Project-code/backend-server2.py
@@ -1,11 +1,11 @@
-#Import Flask Library
+# Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 
-#Initialize the app from Flask
+# Initialize the app from Flask
 app = Flask(__name__)
 
-#Configure MySQL
+# Configure MySQL
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
@@ -13,3 +13,23 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+# Define a route to hello function
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+
+
+app.secret_key = 'some key that you will never guess'
+# Run the app on localhost port 5000
+# debug = True -> you don't have to restart flask
+# for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug=True)
